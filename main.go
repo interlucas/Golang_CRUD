bd0bcba49599b88fabc76599723adf58ae7fb276
interlucas <spinterlucas@gmail.com>
2024-11-20 17:48:16 -0300

    Golang commit 1

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"fmt"
+	_ "github.com/interlucas/Golang_CRUD/docs"
+	"github.com/interlucas/Golang_CRUD/src/configuration/database/mongodb"
+	"github.com/interlucas/Golang_CRUD/src/configuration/logger"
+	"github.com/interlucas/Golang_CRUD/src/controller/routes"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+// @title Meu Primeiro CRUD em Go | HunCoding
+// @version 1.0
+// @description API for crud operations on users
+// @host localhost:8080
+// @BasePath /
+// @schemes http
+// @license MIT
+func main() {
+	//logger.Info("About to start user application")
+
+	godotenv.Load()
+
+	database, err := mongodb.NewMongoDBConnection(context.Background())
+	if err != nil {
+		log.Fatalf(
+			"Error trying to connect to database, error=%s \n",
+			err.Error())
+		return
+	}
+
+	userController := initDependencies(database)
+
+	router := gin.Default()
+	gin.SetMode(gin.ReleaseMode)
+	routes.InitRoutes(&router.RouterGroup, userController)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+}
